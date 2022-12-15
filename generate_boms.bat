d1453da4e953761a8783fad5f8725feab3d419e1
shannon-greenlight <shannon@greenlightdevelopment.com>
2022-12-15 15:01:11 -0800

    Adds BOM

---
+++ generate_boms.bat
@@ -0,0 +1,17 @@
+@echo off
+
+echo This creates BOM files for building the Melper
+echo It requires Melper.xml as an input. Make sure that it is up to date. Use the KiCAD schematic BOM generator with the bom2grouped_csv.xsl plugin that's supplied with KiCAD.
+
+set bin_dir=C:\Program Files\KiCad\bin
+set plugin_dir=C:\Users\shann\Dropbox\KiCAD\BOM
+
+rem "%bin_dir%\python" "%plugin_dir%/bom_lcsc.py" "Melper.xml" "bom\LCSC\Melper_bom.csv"
+
+rem "%bin_dir%\python" "%plugin_dir%/bom_digikey_split.py" "Melper.xml" "bom\Digi-Key\Melper_split.csv"
+
+"%bin_dir%\python" "%plugin_dir%/bom_digikey_full.py" "Melper.xml" "bom\Digi-Key\Melper_full.csv"
+
+if not errorlevel 1 echo Success!
+
+pause
