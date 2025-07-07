14ace8ce5e61dd191a254216380c3644380acac8
Shannon Vance <shannon@greenlightdevelopment.com>
2025-07-07 16:15:06 -0700

    Revision 3.5
    Changes negative regulator.
    Adds support for JLC build.

--- generate_boms.bat
+++ generate_boms.bat
@@ -4,13 +4,13 @@ echo This creates BOM files for building the Melper
 echo It requires Melper.xml as an input. Make sure that it is up to date. Use the KiCAD schematic BOM generator with the bom2grouped_csv.xsl plugin that's supplied with KiCAD.
 
 set bin_dir=C:\Program Files\KiCad\bin
-set plugin_dir=C:\Users\shann\Dropbox\KiCAD\BOM
+set plugin_dir=C:\Users\shann\Dropbox\Python\BOM
 
-rem "%bin_dir%\python" "%plugin_dir%/bom_lcsc.py" "Melper.xml" "bom\LCSC\Melper_bom.csv"
+py "%plugin_dir%/bom_lcsc.py" "Melper.xml" "bom\LCSC\Melper_bom.csv"
 
-rem "%bin_dir%\python" "%plugin_dir%/bom_digikey_split.py" "Melper.xml" "bom\Digi-Key\Melper_split.csv"
+py "%plugin_dir%/bom_digikey_split.py" "Melper.xml" "bom\Digi-Key\Melper_split.csv"
 
-"%bin_dir%\python" "%plugin_dir%/bom_digikey_full.py" "Melper.xml" "bom\Digi-Key\Melper_full.csv"
+py "%plugin_dir%/bom_digikey_full.py" "Melper.xml" "bom\Digi-Key\Melper_full.csv"
 
 if not errorlevel 1 echo Success!
 
